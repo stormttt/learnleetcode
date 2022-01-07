4d4c6b94d9b99179e38d71ab420e37c3ec7a7078
tong <wangtong@WangTongs-MacBook-Pro.local>
2022-01-07 18:39:08 +0800

    learn base binary tree

---
+++ binarytree/preorderTraversaliteratively.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> outPut = new ArrayList<Integer>();
+        if(root == null) return outPut;
+        Stack<TreeNode> saveNode= new Stack<TreeNode>();
+        saveNode.push(root);
+        while(!saveNode.isEmpty()){
+            root = saveNode.pop();
+            outPut.add(root.val);
+            if(root.right != null){
+                saveNode.push(root.right);
+            }
+            if(root.left != null){
+                saveNode.push(root.left);
+            }         
+            
+        }
+        return outPut;
+        
+    }
+}
