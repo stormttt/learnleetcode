09418cb8127cb0f42f548fb3367b501110cdbc88
stromttt <xyxiaoYU1992@gmail.com>
2022-07-18 23:43:26 +0800

    binarysearch

---
+++ 704.binarysearch/binarysearch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if (len(nums) == 0):
+             return -1
+        left = 0
+        right = len(nums) - 1
+        while(left<=right):
+            mid = (left+right)
+            if(nums[mid] == target):
+                return mid
+            elif(nums[mid]<target):
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
